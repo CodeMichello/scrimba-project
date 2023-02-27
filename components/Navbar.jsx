34f4f105cb9cf3da810156c89d9327d009b93287
Michello <michaeladjah.geek@gmail.com>
2023-02-27 15:58:25 +0000

    Initial Commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../Images/react-logo.png" alt="Logo" />
+      <h3>React Facts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
+  )
+  
+}
